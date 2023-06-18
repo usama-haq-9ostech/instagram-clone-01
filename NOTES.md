c5fa7706c9e92d1f0e1142c05b283238fab4d4cf
Usama Haq <usamahaq@9ostech.com>
2023-06-19 00:02:01 +0500

    part 2 done

--- NOTES.md
+++ NOTES.md
@@ -1,9 +1,12 @@
 # Instagram Clone React Native | #1 - Create Home Screen UI
 
-- [Instagram Clone React Native | #1 - Create Home Screen UI](https://www.youtube.com/watch?v=mCBoOpJNGCU)
-- Aug 16, 2021 
+- [Instagram Clone React Native](https://www.youtube.com/playlist?list=PL3JOXEcJ9o8TMjT9F_5-kdVc9D63pctGU)
+  - [Instagram Clone React Native | #1 - Create Home Screen UI](https://www.youtube.com/watch?v=mCBoOpJNGCU) | Aug 16, 2021
+  - [Instagram Clone React Native | #2 - Create Search Screen UI](https://www.youtube.com/watch?v=9uFlwBpLNyw) | Aug 19, 2021
+  - [Instagram Clone React Native | #3 - Instagram Reels](https://www.youtube.com/watch?v=tO6UuiIWHCI) | Sep 6, 2021
+  - [Instagram Clone React Native | #4 - Activity Screen](https://www.youtube.com/watch?v=JgIUwVFArsk) | Sep 11, 2021
+  - [Instagram Clone React Native | #5 - Profile Screen](https://www.youtube.com/watch?v=NiC_XnYrbYs) | Sep 20, 2021
 - [Mr. J](https://www.youtube.com/@mr.j5495)
-
 
 - Install dependencies
   - React Native Navigation
@@ -20,6 +23,3 @@
     - [Google Fonts](https://fonts.google.com/)
   - [React Native Icons](https://www.npmjs.com/package/react-native-vector-icons)
     - `npm i react-native-vector-icons`
-
-- npm install --global expo-cli
-
--- src/components/screens/Search.js
+++ src/components/screens/Search.js
@@ -1,10 +1,145 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  TouchableOpacity,
+  ScrollView,
+  StatusBar,
+  Dimensions,
+  Image,
+  Text,
+} from "react-native";
+import React, { useState } from "react";
+import SearchBox from "../screenComponents/SearchBox";
+import SearchContent from "../screenComponents/SearchContent";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import Ionic from "react-native-vector-icons/Ionicons";
+import Feather from "react-native-vector-icons/Feather";
 
 const Search = () => {
+  const [image, setImage] = useState(null);
+
+  const getData = (data) => {
+    setImage(data);
+  };
+
+  const windowWidth = Dimensions.get("window").width;
+  const windowsHeight = Dimensions.get("window").height;
+
   return (
-    <View>
-      <Text>Search Screen</Text>
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "white",
+        position: "relative",
+      }}
+    >
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <SearchBox />
+        <SearchContent data={getData} />
+        <TouchableOpacity
+          style={{
+            margin: 25,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <AntDesign
+            name="pluscircleo"
+            style={{
+              fontSize: 40,
+              opacity: 0.5,
+            }}
+          />
+        </TouchableOpacity>
+      </ScrollView>
+
+      {image ? (
+        <View
+          style={{
+            position: "absolute",
+            zIndex: 1,
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(52,52,52,0.8)",
+          }}
+        >
+          <StatusBar
+            backgroundColor="#525252"
+            barStyle="dark-content"
+          ></StatusBar>
+          <View
+            style={{
+              position: "absolute",
+              top: windowsHeight / 6,
+              left: windowWidth / 18,
+              backgroundColor: "white",
+              width: 350,
+              height: 465,
+              borderRadius: 15,
+              zIndex: 2,
+              elevation: 50,
+            }}
+          >
+            {/* ---------- Top User Info ---------- */}
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                paddingVertical: 10,
+                paddingHorizontal: 15,
+              }}
+            >
+              <Image
+                source={image}
+                style={{
+                  width: 30,
+                  height: 30,
+                  borderRadius: 100,
+                }}
+              />
+              <View
+                style={{
+                  paddingLeft: 8,
+                }}
+              >
+                <Text style={{ fontSize: 12, fontWeight: "600" }}>
+                  the_anonymous_guy
+                </Text>
+              </View>
+            </View>
+            {/* ---------- Center Image ---------- */}
+            <View>
+              <Image
+                source={image}
+                style={{
+                  width: "100%",
+                  height: 368,
+                }}
+              ></Image>
+            </View>
+            {/* ---------- Bottom Action Buttons ---------- */}
+            <View
+              style={{
+                justifyContent: "space-around",
+                width: "100%",
+                flexDirection: "row",
+                alignItems: "center",
+                flexDirection: "row",
+                padding: 8,
+              }}
+            >
+              <Ionic name="ios-heart-outline" style={{ fontSize: 26 }} />
+              <Ionic
+                name="ios-person-circle-outline"
+                style={{ fontSize: 26 }}
+              />
+              <Feather name="navigation" style={{ fontSize: 26 }} />
+            </View>
+          </View>
+        </View>
+      ) : null}
     </View>
   );
 };
